bf00bc584bf85311eac425934e07594630971ba6
Nookery <nooks@qq.com>
2024-09-03 13:14:39 +0800

    🐛 Fix a bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/JuiceEditor-Demo/',  // 添加这一行
   plugins: [
     vue({
       template: {
